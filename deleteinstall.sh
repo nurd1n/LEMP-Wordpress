5b7fc0b0c16cf96d888dec448cb36faac93e1298
Nurdianto <nurdiantopntr@gmail.com>
2016-02-11 14:44:00 +0700

    Update deleteinstall.sh

--- deleteinstall.sh
+++ deleteinstall.sh
@@ -32,8 +32,6 @@ echo -n "Apa email wordpress yg diinginkan :
 "
 read emailwp
 echo "$emailwp" > deleteemailwp; clear
-#get ip adress
-ifconfig venet0:0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' > deleteipadress.txt
 #install x2go
 apt-get update && apt-get -y install aptitude
 apt-get install gawk python-setuptools software-properties-common
@@ -81,6 +79,8 @@ sed -i 's/post_max_size = 8M/post_max_size = 35M/g' /etc/php5/fpm/php.ini
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 20M/g' /etc/php5/fpm/php.ini
 sed -i 's/max_execution_time = 30/max_execution_time = 600/g' /etc/php5/fpm/php.ini
 wget https://github.com/nurd1n/LEMP-Wordpress/raw/secret/block --no-check-certificate
+#get ip adress
+ifconfig venet0:0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' > deleteipadress.txt
 echo "cat block | sed -e 's/domain/$(cat deletedomain)/g' -e 's/ekstension/$(cat deleteekstension)/g' -e 's/ipadress/$(cat deleteipadress)/g' > /etc/nginx/sites-available/$(cat deletedomain).$(cat deleteekstension)" | bash -
 echo "sudo ln -s /etc/nginx/sites-available/$(cat deletedomain).$(cat deleteekstension) /etc/nginx/sites-enabled/$(cat deletedomain).$(cat deleteekstension)" | bash -
 echo "mkdir -p /home/www/$(cat deletedomain)" | bash -
