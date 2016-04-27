d3193d3063277ec2be50e415b1dfce38a66b8e3e
Nurdianto <nurdiantopntr@gmail.com>
2016-04-27 08:50:58 +0700

    Update clone-antar-vps.sh

--- clone-antar-vps.sh
+++ clone-antar-vps.sh
@@ -20,6 +20,10 @@ echo -n "Apa password database wordpress lama :
 "
 read passdbawal
 echo "$passdbawal" > deletepassdbawal; clear
+echo -n "Apa inisial blog lama anda (huruf kecil semua) :
+"
+read inisialawal
+echo "$inisialawal" > deleteinisialawal; clear
 echo -n "Apa nama domain baru anda (tanpa dot com) :
 "
 read domainbaru
@@ -33,6 +37,10 @@ echo -n "Apa password mysql yg anda inginkan (huruf dan angka) :
 "
 read passmysql
 echo "$passmysql" > deletepassmysql; clear
+echo -n "Apa inisial blog baru anda (huruf kecil semua) :
+"
+read inisialbaru
+echo "$inisialbaru" > deleteinisialbaru; clear
 tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1 > deleteuserdbbaru; clear
 tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1 > deletepassdbbaru; clear
 echo "user db wp : $(cat /deleteuserdbbaru)" >> /home/database.txt
@@ -53,7 +61,20 @@ echo "sed -e 's|$(cat /deletedomainawal)|$(cat /deletedomainbaru)|g' -e 's|$(cat
 rm -f wp-config.php
 mv wp-config2.php wp-config.php
 echo "sed -i 's|$(cat /deletedomainawal).$(cat /deleteekstensionawal)|$(cat /deletedomainbaru).$(cat /deleteekstensionbaru)|g' robots.txt" | bash -
-echo "curl -L http://$(cat /deleteipawal)/wp_$(cat /deletedomainawal)$(cat /deleteekstensionawal).sql | sed -e 's/$(cat /deletedomainawal).$(cat /deleteekstensionawal)/$(cat /deletedomainbaru).$(cat /deleteekstensionbaru)/g' -e 's/$(cat /deletedomainawal)/$(cat /deletedomainbaru)/g' > wp_$(cat /deletedomainbaru).sql" | bash -
+echo "curl -L http://$(cat /deleteipawal)/wp_$(cat /deletedomainawal)$(cat /deleteekstensionawal).sql > wp_$(cat /deletedomainbaru).sql" | bash -
+echo "UPDATE wp_posts SET post_content = replace(post_content,\"$(cat /deletedomainawal).$(cat /deleteekstensionawal)\",\"$(cat /deletedomainbaru).$(cat /deleteekstensionbaru)\");" > deletemysql.sql
+echo "UPDATE wp_posts SET post_content = replace(post_content,\"$(cat /deletedomainawal)\",\"$(cat /deletedomainbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_posts SET post_content = replace(post_content,\"$(cat /deleteinisialawal)\",\"$(cat /deleteinisialbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_posts SET post_content = replace(post_content,\"$(cat /deleteinisialawal | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\",\"$(cat /deleteinisialbaru | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\");" >> deletemysql.sql
+echo "UPDATE wp_postmeta SET meta_value = replace(meta_value,\"$(cat /deletedomainawal).$(cat /deleteekstensionawal)\",\"$(cat /deletedomainbaru).$(cat /deleteekstensionbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_postmeta SET meta_value = replace(meta_value,\"$(cat /deletedomainawal)\",\"$(cat /deletedomainbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_postmeta SET meta_value = replace(meta_value,\"$(cat /deleteinisialawal)\",\"$(cat /deleteinisialbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_postmeta SET meta_value = replace(meta_value,\"$(cat /deleteinisialawal | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\",\"$(cat /deleteinisialbaru | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\");" >> deletemysql.sql
+echo "UPDATE wp_options SET option_value = replace(option_value,\"$(cat /deletedomainawal).$(cat /deleteekstensionawal)\",\"$(cat /deletedomainbaru).$(cat /deleteekstensionbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_options SET option_value = replace(option_value,\"$(cat /deletedomainawal)\",\"$(cat /deletedomainbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_options SET option_value = replace(option_value,\"$(cat /deleteinisialawal)\",\"$(cat /deleteinisialbaru)\");" >> deletemysql.sql
+echo "UPDATE wp_options SET option_value = replace(option_value,\"$(cat /deleteinisialawal | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\",\"$(cat /deleteinisialbaru | sed -e 's/+/ /g' -e 's/.*/\L&/; s/[a-z]*/\u&/g')\");" >> deletemysql.sql
+wp db query --allow-root < deletemysql.sql
 chown -R www-data:www-data *
 # Create database, ganti password, wordpressdb
 echo "echo \"echo \\\"create database wp_\$(cat /deletedomainbaru); create user \$(cat /deleteuserdbbaru)@localhost identified by '\$(cat /deletepassdbbaru)'; grant all privileges on wp_\$(cat /deletedomainbaru).* to \$(cat /deleteuserdbbaru)@localhost identified by '\$(cat /deletepassdbbaru)'; flush privileges\\\" | mysql -u root \\\"-p\$(cat /deletepassmysql)\\\"\"" | bash - | bash -
@@ -62,9 +83,11 @@ echo "rm -f wp_$(cat /deletedomainbaru).sql" | bash -
 rm -f domain.tar.gz
 echo "UPDATE \`wp_posts\` SET \`post_status\` = 'draft' where \`post_status\` = 'publish' and \`post_type\` = 'post'; UPDATE \`wp_posts\` SET \`post_status\` = 'draft' where \`post_status\` = 'future' and \`post_type\` = 'post';" > deletemysql.sql
 wp db query --allow-root < deletemysql.sql
-wp plugin install drafts-scheduler --activate --allow-root
-wp plugin install wp-missed-schedule --activate --allow-root
+wp plugin install drafts-scheduler --allow-root
+wp plugin activate drafts-scheduler --allow-root
+wp plugin install https://github.com/sLaNGjI/wp-missed-schedule/archive/master.zip --activate --allow-root
 wp plugin update --all --allow-root
 rm -f deletemysql.sql
+wp core update --version=4.5 --force --allow-root
 wp core update-db --allow-root
 wp plugin delete no-ping-wait wordpress-ping-optimizer wp-limit-login-attempts --allow-root
