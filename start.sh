8590bcb03dc22d7b952f6d87fbc9457615b40f10
Nurdianto <nurdiantopntr@gmail.com>
2016-07-30 08:18:20 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while read f1; do
-    echo "sed -n '$f1' < data/domain > /deletedomain" | bash -
-    echo "sed -n '$f1' < data/ekstension > /deleteekstension" | bash -
-    echo "sed -n '$f1' < data/keyword > /deletekeyword" | bash -    
-    echo "sed -n '$f1' < data/niches > /deleteniches" | bash -    
-    echo "cat data/passmysql > /deletepassmysql" | bash -
-    echo "source build.sh" | bash -
-done < "start.txt"
