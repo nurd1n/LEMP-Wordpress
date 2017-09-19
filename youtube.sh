802881989292ab7868ec905b49b4487eb2167ccd
Nurdianto <nurdiantopntr@gmail.com>
2017-09-19 14:50:22 +0700

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -27,6 +27,7 @@ mkdir /root/upload
 cd /root/upload
 wget -q --no-check-certificate https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 tar xf ffmpeg-release-64bit-static.tar.xz
+rm -f *.tar.xz
 mv ffmpeg* ffmpeg-tool
 cp ffmpeg-tool/ffmpeg /usr/bin/ffmpeg2
 cp ffmpeg-tool/ffprobe /usr/bin/ffprobe2
